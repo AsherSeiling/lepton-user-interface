be028e1362041ccd98dc756daf8b624f72835136
Asher Seiling <f14asher@gmail.com>
2021-03-12 12:53:46 -0600

    Added the git ignore

--- compress_files.py
+++ compress_files.py
@@ -1,5 +1,6 @@
 import os
 def compress_single_file(url, save_url):
-	name, exstension = os.path.splitext(url)
-	os.system(f"./lepton/build/lepton {url} {save_url}{name}.lep")
+	url1 = os.path.basename(url)
+	name, exstension = os.path.splitext(url1)
+	os.system(f"./lepton/build/lepton {url} {save_url}/{name}.lep")
 	
